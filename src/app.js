c69ca582a948ab380d696198c2a792dae937220b
RekaMel <63839716+RekaMel@users.noreply.github.com>
2023-09-16 19:47:47 +0200

    Updated icon size and position

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col">
+      <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
